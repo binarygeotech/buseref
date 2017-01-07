6a9a0903bbdbd13598bbdfe1fd59062c4d6bc77c
George Okojie Davis <binarygeotech@gmail.com>
2017-01-07 22:21:32 +0100

    Added Option to accept filename instead of file content
    Accepts initialization using ```buseref.file(filename, options)```

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ module.exports = function (content, options) {
 
   return [ transformedContent, replaced ];
 };
+
+module.exports.file = function (filename, options) {
+	var e = fs.existsSync(filename);
+	
+	if(e){
+		var content = fs.readFileSync(filename, 'utf8');
+		
+		if(content.length) {
+			var blocks = getBlocks(content),
+			opts = options || {},
+			transformedContent = transformReferences(blocks, content, opts),
+			replaced = compactContent(blocks);
+			
+			return [ transformedContent, replaced ];
+		}
+	}
+	
+	return null;
+};
